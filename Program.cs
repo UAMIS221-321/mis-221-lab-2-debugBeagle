c8960efcc976aa514903364d316cf6a569f08645
Dan Douglas <dandouglas@Dans-MacBook-Air.local>
2024-09-09 10:58:48 -0500

    final

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//varibles 
+int numberOfSandwiches = 0;
+int numberOfToppings = 0;
+double tip = 0;
+
+double totalSandwichCost = 0;
+double totalToppingCost = 0;
+double baseCost = 0;
+double orderCost = 0;
+
+const double COST_OF_SANDWICH = 4.75;
+const double COST_OF_TOPPING = 0.55;
+const double DISCOUNT_AMOUNT = 0.10;
+
+
+System.Console.WriteLine("how many sandwiches?");
+numberOfSandwiches = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("how many toppings?");
+numberOfToppings = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("How much tip would you like to leave?");
+tip = double.Parse(Console.ReadLine());
+
+System.Console.WriteLine("So its " + numberOfSandwiches + " sandwiches " + numberOfToppings + " toppings and a " + tip + " tip thank you ");
+
+totalSandwichCost = numberOfSandwiches * COST_OF_SANDWICH;
+totalToppingCost = COST_OF_TOPPING * numberOfToppings;
+baseCost = totalSandwichCost + totalToppingCost;
+
+orderCost = tip + baseCost * (1 - DISCOUNT_AMOUNT);
+
+System.Console.WriteLine("Your total is $" + orderCost);
+
+
